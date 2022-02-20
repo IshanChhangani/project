f6023fd64845284c868f492f7f3f4bf5432dd4cd
IshanChhangani <91928381+IshanChhangani@users.noreply.github.com>
2022-02-21 01:21:58 +0530

    Initial commit

---
+++ Server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+const db = require("./models");
+
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
+});
---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Link to="/createpost"> Create A Post</Link>
+        <Link to="/"> Home Page</Link>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/createpost" exact component={CreatePost} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
